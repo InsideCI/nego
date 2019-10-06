209d451e424c0ed3f548e173702de6adde08ea74
Cleanderson Lins <cleandersonlins@gmail.com>
2019-10-05 21:19:11 -0300

    Routes, store and .env files.

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-func init() {
-	st = model.NewStudant("111", "Klean", "312321")
-	fmt.Println(st)
+	"github.com/InsideCI/nego/routes"
+	"github.com/gorilla/mux"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!")
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/students/{id}", routes.GetStudent).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
+
+func main() {
+	fmt.Println("Go ORM tutorial.")
+
+	handleRequests()
 }
