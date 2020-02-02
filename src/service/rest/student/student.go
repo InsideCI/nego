2cc7b365317a9af437f2ab1ce2c82b04841deed3
kleanlins <cleandersonlins@gmail.com>
2020-02-02 11:02:02 -0300

    Major changes in project structure for a more reliable and dynamic approach.

--- src/service/rest/student/student.go
+++ src/service/rest/student/student.go
@@ -6,25 +6,25 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/InsideCI/nego/driver"
-	"github.com/InsideCI/nego/model"
-	"github.com/InsideCI/nego/repository"
-	"github.com/InsideCI/nego/repository/student"
+	"github.com/InsideCI/nego/src/driver"
+	"github.com/InsideCI/nego/src/model"
+	"github.com/InsideCI/nego/src/repository"
+	"github.com/InsideCI/nego/src/repository/student"
 )
 
-// Student is a handler that wraps a Student repository.
-type Student struct {
+// StudentService is a service that wraps a StudentRepository.
+type StudentService struct {
 	repo repository.StudentRepository
 }
 
-// NewStudentHandler creates a new handler with a specific database engine.
-func NewStudentHandler(db *driver.DB) *Student {
-	return &Student{
+// NewStudentService creates a new service with a specific database engine.
+func NewStudentService(db *driver.DB) *StudentService {
+	return &StudentService{
 		repo: student.NewStudentRepository(db),
 	}
 }
 
-func (s *Student) Create(w http.ResponseWriter, r *http.Request) {
+func (s *StudentService) Create(w http.ResponseWriter, r *http.Request) {
 	var created int
 	ctx := r.Context()
 	students := ctx.Value("students").([]model.Student)
@@ -38,7 +38,7 @@ func (s *Student) Create(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(strconv.Itoa(created)))
 }
 
-func (s *Student) Fetch(w http.ResponseWriter, r *http.Request) {
+func (s *StudentService) Fetch(w http.ResponseWriter, r *http.Request) {
 	students, err := s.repo.Fetch()
 	if err != nil {
 		log.Println("[HANDLER]", err)
@@ -48,7 +48,7 @@ func (s *Student) Fetch(w http.ResponseWriter, r *http.Request) {
 }
 
 // FetchOne uses id param as primary key search
-func (s *Student) FetchOne(w http.ResponseWriter, r *http.Request) {
+func (s *StudentService) FetchOne(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	registration, _ := strconv.Atoi(ctx.Value("id").(string))
 
