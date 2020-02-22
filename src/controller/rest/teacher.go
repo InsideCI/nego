53a4a91bc344bcae4f8ce86b3837c4085be83f3b
kleanlins <cleandersonlins@gmail.com>
2020-02-22 19:19:59 -0300

    Generic repository fixed.

--- src/controller/rest/teacher.go
+++ src/controller/rest/teacher.go
@@ -5,6 +5,8 @@ import (
 	"github.com/InsideCI/nego/src/driver"
 	"github.com/InsideCI/nego/src/model"
 	"github.com/InsideCI/nego/src/services"
+	"github.com/InsideCI/nego/src/utils"
+	"github.com/InsideCI/nego/src/utils/exceptions"
 	"log"
 	"net/http"
 )
@@ -21,14 +23,29 @@ func NewTeacherController(db *driver.DB) *TeacherController {
 	}
 }
 
-func (s *TeacherController) Create(w http.ResponseWriter, r *http.Request) {
+func (c *TeacherController) Create(w http.ResponseWriter, r *http.Request) {
 	teacher := r.Context().Value("payload").(model.Teacher)
 
-	created, err := s.service.Create(s.db, teacher)
+	created, err := c.service.Create(c.db, teacher)
 
 	if err != nil {
 		log.Println("[HANDLER]", err)
+		utils.Throw(w, exceptions.BadRequest, err)
+		return
 	}
 
 	_ = json.NewEncoder(w).Encode(created)
+}
+
+func (c *TeacherController) FetchOne(w http.ResponseWriter, r *http.Request) {
+	registration := r.Context().Value("id").(string)
+
+	student, err := c.service.FetchOne(c.db, registration)
+	if err != nil {
+		utils.Throw(w, exceptions.InvalidAttributes, err)
+		return
+	}
+
+	_ = json.NewEncoder(w).Encode(student)
+
 }
--- src/repository/student.go
+++ src/repository/student.go
@@ -8,11 +8,14 @@ import (
 )
 
 type StudentRepository struct {
+	GenericRepository
 }
 
 // NewStudentRepository creates a PostgreSQL CRUD interface implementation
 func NewStudentRepository() *StudentRepository {
-	return &StudentRepository{}
+	return &StudentRepository{
+		struct{ Type interface{} }{Type: model.Student{}},
+	}
 }
 
 func (r *StudentRepository) FetchByName(db *gorm.DB, name string) (*[]model.Student, error) {
@@ -32,51 +35,4 @@ func (r *StudentRepository) FetchByRegistration(db *gorm.DB, registration string
 		return nil, err
 	}
 	return &student, nil
-}
-
-func (r *StudentRepository) Create(db *gorm.DB, student *model.Student) (*model.Student, error) {
-	if err := db.Create(&student).Error; err != nil {
-		return nil, err
-	}
-	return student, nil
-}
-
-func (r *StudentRepository) Count(db *gorm.DB) (int, error) {
-	var count int
-	if err := db.Model(&model.Student{}).Count(&count).Error; err != nil {
-		return 0, err
-	}
-	return count, nil
-}
-
-func (r *StudentRepository) Fetch(db *gorm.DB, limit int) (*[]model.Student, error) {
-	var students []model.Student
-	if err := db.Limit(limit).Order("nome").Find(&students).Error; err != nil {
-		return nil, err
-	}
-	return &students, nil
-}
-
-func (r *StudentRepository) FetchOne(db *gorm.DB, registration string) (*model.Student, error) {
-	var student model.Student
-	err := db.Where("matricula = ?", registration).First(&student).Error
-	if err != nil {
-		return nil, err
-	}
-	return &student, nil
-}
-
-func (r *StudentRepository) Exists(db *gorm.DB, id int) (bool, error) {
-	var count int
-	if err := db.Model(&model.Student{Matricula: id}).Count(&count).Error; err != nil {
-		return false, err
-	}
-	return count != 0, nil
-}
-
-func (r *StudentRepository) Delete(db *gorm.DB, id int) (bool, error) {
-	if err := db.Delete(&model.Student{Matricula: id}).Error; err != nil {
-		return false, err
-	}
-	return true, nil
 }
